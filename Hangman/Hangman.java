b3b206ec84ef98c4cf06b1ef4eb2d6d1852ee350
Timothy Goll <timothyderasiat@gmail.com>
2025-01-15 15:46:00 +0100

    gui für Haengerman

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -3,11 +3,11 @@ import javax.swing.*;
 import java.awt.*;
 
 
-
 public class Hangman {
-    private JPanel mainPanel, buttonPanel, hangerPanel, namePanel;
+    private JPanel mainPanel, buttonPanel, eingabePanel;
     private JButton home;
     private JTextField eingabeBuchstaben, falscheBuchstaben;
+    private JLabel titleLabel, uescrhiftEins, ueschriftZwei;
     private JFrame frame;
 
     public Hangman(){
@@ -17,13 +17,34 @@ public class Hangman {
         frame.setLayout(new BorderLayout());
 
         mainPanel = new JPanel(); 
+        mainPanel.setPreferredSize(new Dimension(400, 100));
         buttonPanel = new JPanel();
-        hangerPanel = new JPanel();
-        namePanel = new JPanel();
+        eingabePanel = new JPanel(new GridLayout(2,2));
 
-        JLabel titleLabel = new JLabel("Hangman", SwingConstants.CENTER);
+        eingabeBuchstaben = new JTextField();
+        falscheBuchstaben = new JTextField();
+        falscheBuchstaben.setEnabled(false);
+
+        home = new JButton("Home");
+
+        titleLabel = new JLabel("Hangman", SwingConstants.CENTER);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        frame.add(titleLabel, BorderLayout.NORTH);
+        uescrhiftEins = new JLabel("Eingabe:", SwingConstants.CENTER);
+        uescrhiftEins.setFont(new Font("Arial", Font.BOLD, 12));
+        ueschriftZwei = new JLabel("Nicht gültige Buchstaben", SwingConstants.CENTER);
+        ueschriftZwei.setFont(new Font("Arial", Font.BOLD, 12));
+
+        eingabePanel.add(uescrhiftEins);
+        eingabePanel.add(ueschriftZwei);
+        eingabePanel.add(eingabeBuchstaben);
+        eingabePanel.add(falscheBuchstaben);
+
+        buttonPanel.add(titleLabel);
+        buttonPanel.add(home);
+
+        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.NORTH);
+        frame.add(eingabePanel, BorderLayout.SOUTH);
         frame.setLocationRelativeTo(null);
     
         frame.setVisible(true);
