4d0930d3d512fc5a67ef5743574360613b4abdaa
Timothy Goll <timothyderasiat@gmail.com>
2025-02-19 15:45:55 +0100

    Gui verfeinert

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -7,40 +7,49 @@ public class Hangman {
     private JPanel mainPanel, buttonPanel, eingabePanel;
     private JButton home;
     private JTextField eingabeBuchstaben, falscheBuchstaben;
-    private JLabel titleLabel, uescrhiftEins, ueschriftZwei;
+    private JLabel titleLabel, uescrhiftEins, ueschriftZwei, Jwort, man, wort;
     private JFrame frame;
 
     public Hangman(){
         frame = new JFrame("Startmenü");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(700, 500);
         frame.setLayout(new BorderLayout());
 
         mainPanel = new JPanel(); 
         mainPanel.setPreferredSize(new Dimension(400, 100));
-        buttonPanel = new JPanel();
-        eingabePanel = new JPanel(new GridLayout(2,2));
+        buttonPanel = new JPanel(new GridLayout(2,3));
+        eingabePanel = new JPanel(new GridLayout(2,2,80,10));
 
         eingabeBuchstaben = new JTextField();
         falscheBuchstaben = new JTextField();
         falscheBuchstaben.setEnabled(false);
-
+        wort = new JLabel("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _", SwingConstants.CENTER);
+        wort.setEnabled(false);
+        Jwort = new JLabel("Wort:",SwingConstants.CENTER);
+        Jwort.setFont(new Font("Arial",Font.BOLD,12));
         home = new JButton("Home");
 
-        titleLabel = new JLabel("Hangman", SwingConstants.CENTER);
+        titleLabel = new JLabel("Hang", SwingConstants.RIGHT);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        man = new JLabel("man", SwingConstants.LEFT);
+        man.setFont(new Font("Arial", Font.BOLD, 24));
         uescrhiftEins = new JLabel("Eingabe:", SwingConstants.CENTER);
         uescrhiftEins.setFont(new Font("Arial", Font.BOLD, 12));
         ueschriftZwei = new JLabel("Nicht gültige Buchstaben", SwingConstants.CENTER);
         ueschriftZwei.setFont(new Font("Arial", Font.BOLD, 12));
 
         eingabePanel.add(uescrhiftEins);
+        eingabePanel.add(new JPanel());
         eingabePanel.add(ueschriftZwei);
         eingabePanel.add(eingabeBuchstaben);
+        eingabePanel.add(home);
         eingabePanel.add(falscheBuchstaben);
 
         buttonPanel.add(titleLabel);
-        buttonPanel.add(home);
+        buttonPanel.add(man);
+        buttonPanel.add(Jwort);
+        buttonPanel.add(wort);
 
         frame.add(mainPanel, BorderLayout.CENTER);
         frame.add(buttonPanel, BorderLayout.NORTH);
